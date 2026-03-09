2ba78c888896c3571d81d69698aca2a2ce53a99c
Lanz Magick <93784560+Lanzkila@users.noreply.github.com>
2026-03-10 03:56:15 +0800

    Update ui-elements.js

--- ui-elements.js
+++ ui-elements.js
@@ -3,7 +3,10 @@ const toolbar = `
         <button class="btn-nav" onclick="changePage(-1)">PREV</button>
         <div id="pageCounter">Page 0 / 0</div>
         <button class="btn-nav" onclick="changePage(1)">NEXT</button>
-        <button class="btn-mode" id="modeBtn" onclick="toggleDouble()">2-PAGE</button>
+        
+        <button class="btn-mode" id="doubleBtn" onclick="toggleDouble()">2-PAGE: ON</button>
+        
+        <button class="btn-mode" id="webtoonBtn" onclick="toggleWebtoon()">WEBTOON: OFF</button>
         
         <input type="file" id="fileInput" hidden onchange="handleFile(this.files[0])">
         <label for="fileInput" class="btn-upload">OPEN</label>
